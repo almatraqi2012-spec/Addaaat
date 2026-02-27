eb6c6a56d8e90b2a17aec0f0f617a35bfbb9fea0
almatraqi2012-spec <almatraqi2012@gmail.com>
2026-02-27 15:35:22 +0300

    Update aladafmadfa.py

--- aladafmadfa.py
+++ aladafmadfa.py
@@ -7,7 +7,7 @@ from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRe
 from telethon.errors import SessionPasswordNeededError, FloodWaitError, UserPrivacyRestrictedError, PeerFloodError
 
 # ================= [ ⚙️ إعداداتك ] =================
-BOT_TOKEN = "8774804527:AAHaCMOst4XZVpowd6lw483gsUZuIlHkXlY"
+BOT_TOKEN = "8574116889:AAFU30-IOr522e_y1H7NW5V_hN4R3yXMExg"
 MY_API_ID = 23269382
 MY_API_HASH = 'fe19c565fb4378bd5128885428ff8e26'
 ADMIN_ID = 6016547718

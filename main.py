9112a2d82860562f8614c6855a9b08b0f72574d8
almatraqi2012-spec <almatraqi2012@gmail.com>
2026-05-03 20:05:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from telethon import TelegramClient, functions, types as tl_types, errors
 from telethon.tl.functions.channels import JoinChannelRequest, InviteToChannelRequest
 from flask import Flask, request
 # ================= [ ⚙️ الإعدادات المركزية ] ================
-BOT_TOKEN = "8574116889:AAHSlnMQE442Y_RWH5hYq4wNcJkOw2LiArM"
+BOT_TOKEN = "8791690196:AAGvZp93UlVdiw-afcZHaF03n46v-OA-PW8"
 MY_API_ID = 21349867
 MY_API_HASH = '7ced3ee4c80117bd5138410811b91f9f'
 ADMIN_ID = 6016547718
